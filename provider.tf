133e77c3060a300f0ec73153e30959734f84be55
James Mason <james.mason@capgemini.com>
2020-01-16 11:36:23 +0000

    updated AZ

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   version = "=1.38.0"
+
+  subscription_id = "560b96a8-a22f-4d9f-ba3d-e571f78811a4"
+  client_id       = "305034cb-c5d4-4052-9dbf-2b5f518a65ef"
+  client_secret   = "d5e9e20b-842b-4d7a-b67d-4ca117525d15"
+  tenant_id       = "ab2991ed-49bc-4864-9116-c895d24ca7d5"
 }
